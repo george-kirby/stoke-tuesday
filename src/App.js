2741231d0f468ef230371ad0b1210905b87917e6
george-kirby <georgeskirby@btinternet.com>
2019-12-03 17:18:31 +0000

    quick title to test App.js rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="main-header">But can they do it on a cold, wet, Tuesday night in Stoke?</h2>
     </div>
   );
 }
