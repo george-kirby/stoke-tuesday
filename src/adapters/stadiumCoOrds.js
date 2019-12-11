70a1a61e40ae3ba4d0223d4382075fbd6bb785d5
george-kirby <georgeskirby@btinternet.com>
2019-12-11 13:24:12 +0000

    updated keys to match FootballData venue names

--- src/adapters/stadiumCoOrds.js
+++ src/adapters/stadiumCoOrds.js
@@ -1,25 +1,24 @@
 const stadiumCoOrds = {
     "Anfield": ["53.430932", "-2.960444"],
-    "King Power": ["52.620536", "-1.142125"],
-    "Etihad": ["53.483317", "-2.200374"],
+    "King Power Stadium": ["52.620536", "-1.142125"],
+    "Etihad Stadium": ["53.483317", "-2.200374"],
     "Stamford Bridge": ["51.481850", "-0.190924"],
     "Tottenham Hotspur Stadium": ["51.604392", "-0.066371"],
-    "Wembley": ["51.556201", "-0.279530"],
     "Bramall Lane": ["53.370534", "-1.470880"],
-    "Molineux": ["52.590412", "-2.130386"],
-    "Emirates": ["51.555082", "-0.108417"],
+    "Molineux Stadium": ["52.590412", "-2.130386"],
+    "Emirates Stadium": ["51.555082", "-0.108417"],
     "Old Trafford": ["53.463909", "-2.291373"],
     "Turf Moor": ["53.789093", "-2.230219"],
     "Selhurst Park": ["51.398349", "-0.085420"],
     "Vitality Stadium": ["50.735414", "-1.838268"],
     "London Stadium": ["51.538883", "-0.016604"],
-    "St James' Park": ["54.975716", "-1.621667"],
+    "St. James' Park": ["54.975716", "-1.621667"],
     "Villa Park": ["52.509157", "-1.884780"],
-    "Amex": ["50.861734", "-0.083727"],
+    "The American Express Community Stadium": ["50.861734", "-0.083727"],
     "Goodison Park": ["53.438947", "-2.966266"],
-    "St Mary's": ["50.906042", "-1.390919"],
+    "St. Mary's Stadium": ["50.906042", "-1.390919"],
     "Carrow Road": ["52.622336", "1.309317"],
-    "Vicarage Road": ["51.650112", "-0.401514"] 
+    "Vicarage Road Stadium": ["51.650112", "-0.401514"] 
 }
 
 export default stadiumCoOrds
