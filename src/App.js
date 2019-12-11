ce5d691c84a2ac52de49cfc471c7cb8b2c1efc64
george-kirby <georgeskirby@btinternet.com>
2019-12-11 11:24:47 +0000

    testing API matches endpoint with useEffect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import APIFootball from './adapters/FootballDataAPI'
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    APIFootball.getResultsByTeamId(73)
+  }, []);
+
   return (
     <div>
       <h2 className="main-header">But can they do it on a cold, wet, Tuesday night in Stoke?</h2>
