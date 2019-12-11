558e139332c490abb5b7f9c53f1ecb9bdd2eaa44
george-kirby <georgeskirby@btinternet.com>
2019-12-11 14:21:33 +0000

    removed Dark Sky API key

--- src/adapters/DarkSkyAPI.js
+++ src/adapters/DarkSkyAPI.js
@@ -1,4 +1,6 @@
-const DARK_SKY_KEY = "012d1b3ba5ff93b6a3ceaf3e91788c2e"
+import Keys from './Keys'
+
+const DARK_SKY_KEY = Keys.REACT_APP_KEY_DARK_SKY
 const DARK_SKY_API_ENDPOINT = "https://api.darksky.net/"
 const FORECAST_ENDPOINT = `${DARK_SKY_API_ENDPOINT}forecast/`
 
